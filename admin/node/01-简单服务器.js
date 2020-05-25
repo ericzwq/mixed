c4b77bd06c6b3518df30d09e82ad72c64fdfe500
Eric <1872757047@qq.com>
2020-05-25 09:51:03 +0800

    换地方了

---
+++ admin/node/01-简单服务器.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var server = http.createServer();
+server.on('request', function (req, res) {
+    var url = req.url.split('?')[0];
+    console.log(url);
+    if (url == '/index.html' || url == '/login.html' || url == '/category.html') {
+        res.write('<h1>' + url + '</h1>');
+    } else {
+        res.write('<h1>Sorry! It is not find.</h1>');
+    }
+    res.end('a');
+});
+server.listen(3000, function () {
+    console.log('start successful')
+});
---
+++ admin/node/feedback/app.js
@@ -0,0 +1,72 @@
+var tpl = require('art-template');
+var fs = require('fs');
+var url = require('url');
+var comments = [
+    {
+        name: '李规划1',
+        message: '我真帅',
+        time: '34343-34-34'
+    },
+    {
+        name: '李规划2',
+        message: '我真帅',
+        time: '34343-34-34'
+    },
+    {
+        name: '李规划3',
+        message: '我真帅',
+        time: '34343-34-34'
+    },
+    {
+        name: '李规划4',
+        message: '我真帅',
+        time: '34343-34-34'
+    }
+];
+require('http').createServer(function (req, res) {
+    var parseUrl = url.parse(req.url, true);
+    var path = parseUrl.pathname;
+    var query = parseUrl.query;
+    if (path === '/') {
+
+        fs.readFile('./views/index.html', function (err, data) {
+            if (err) res.end('busy');
+            var s = tpl.render(data.toString(), {data: comments});
+            res.end(s);
+        })
+    } else if (path.indexOf('/public/') === 0) {
+        fs.readFile('.' + req.url, function (err, data) {
+            if (err) {
+                res.end('not found');
+                return false
+            }
+            res.end(data.toString())
+        })
+    } else if (path === '/post') {
+        fs.readFile('views/post.html', function (err, data) {
+            if (err) {
+                res.end('busy');
+                return false;
+            }
+            res.end(data.toString());
+        })
+    } else if (path === '/comments') {
+        if (JSON.stringify(query) !== '{}') {
+            comments.push({name: query.name, message: query.message, time: Date.now()});
+        }
+        //重定向 302为临时重定向 301永久重定向
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+    } else {
+        fs.readFile('views/404.html', function (err, data) {
+            if (err) {
+                res.end('404 busy');
+                return false;
+            }
+            res.end(data.toString());
+        })
+    }
+}).listen(3000, function () {
+    console.log('success')
+});
