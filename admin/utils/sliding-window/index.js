35a30bba9a73b0a5bd2e1c991ca0ac808d1a00ff
eric <1872757047@qq.com>
2021-12-02 20:49:43 +0800

    solution

--- admin/utils/sliding-window/index.js
+++ admin/utils/sliding-window/index.js
@@ -56,3 +56,43 @@ console.log(slidingWindow.leftMove())
 console.log(slidingWindow.rightMove())
 
 console.log(slidingWindow)
+
+// ---------------------------------------------------------------------------------------------------------------------
+const MinStack = function () { // min函数，任意使用pop，push，O(1)时间复杂度求当前数据data中最小值
+  this.data = []
+  this.win = []
+};
+
+MinStack.prototype.push = function (x) {
+  let {data, win} = this
+  data.push(x)
+  if (!win.length || win[win.length - 1] >= x) win.push(x) // 从大到小保留
+};
+
+MinStack.prototype.pop = function () {
+  let {data, win} = this
+  if (!data.length) return null
+  let res = data.pop()
+  if (win[win.length - 1] === res) win.pop()
+};
+
+MinStack.prototype.top = function () {
+  return this.data.length ? this.data[this.data.length - 1] : null
+};
+
+MinStack.prototype.min = function () {
+  return this.win.length ? this.win[this.win.length - 1] : null
+};
+
+let minStack = new MinStack();
+minStack.push(2);
+minStack.push(0);
+minStack.push(3);
+minStack.push(0);
+console.log(minStack.min());
+console.log(minStack.pop());
+console.log(minStack.min());
+console.log(minStack.pop());
+console.log(minStack.min());
+console.log(minStack.pop());
+console.log(minStack.min());
